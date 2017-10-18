053944983dffc45b75505a327ecff21d565ba7a0
Teimuraz <ttrapaid@terpmail.umd.edu>
2017-10-18 14:20:21 -0400

    first test code

---
+++ new.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "hello world" << endl;
+  int i=2;
+  cout << "i = " <<i<<endl;
+
+  double a=3.3;
+  cout << "a = " <<a<<endl;
+
+  int j = a * i;
+
+  cout << "a*i = " <<j<<endl;
+ 
+  return 0;
+
+}
