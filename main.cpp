4c8c52a6073402e9792fc9fe31dd63fa35be3114
Teimuraz <ttrapaid@terpmail.umd.edu>
2017-10-18 14:24:22 -0400

    Hello World Code

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout <<"Hello World!" <<endl;   //Print hello world to screen followed by end line (endl)
+  return 0;        //exit the program
+}
