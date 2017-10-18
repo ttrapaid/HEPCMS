604e89390bfabe9f5aa2a73b315d6ed1e10f83a6
Teimuraz <ttrapaid@terpmail.umd.edu>
2017-10-18 14:29:46 -0400

    More practice code

---
+++ operations.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n = 10;
+  
+  cout<<"n is " <<n<<endl;
+
+  n--;
+  
+  cout<<"n is now " <<n<<endl;
+
+  n++;
+  
+  cout<<"n is now " <<n<<endl;
+
+  return 0;
+
+}
