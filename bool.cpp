2bfa2a5c2853db4d07702ba61b73566afc2db34a
Teimuraz <ttrapaid@terpmail.umd.edu>
2017-10-18 14:35:05 -0400

    logic code

---
+++ bool.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  bool prop;
+  prop = (5 > 1);
+
+  cout<<"prop is "<<prop<<endl;
+
+  prop = (1>5);
+
+  cout<<"prop is "<<prop<<endl;
+
+  prop = (1 != 5);
+
+  cout<<"prop is "<<prop<<endl;
+
+  return 0;
+
+}
